a628e94d95f515877e2359d2a36a726b62f32a7b
Eduardo Ramos <eduardorfreitas93@gmail.com>
2021-02-22 10:33:18 -0300

    Removendo console.log

--- src/utils/FilterMultimedia.js
+++ src/utils/FilterMultimedia.js
@@ -3,7 +3,7 @@ import {useCallback} from 'react';
 function FilterMultimedia() {
   return useCallback((item) => {
     if (!item) return null;
-console.log('aquiiiii');
+
     return item.multimedia.filter((item) => item.format === 'superJumbo')[0];
   }, []);
 }
